dd0979a1def631acccedf4c30d0028154bf2cbe7
yunkyu <ckyun777@gmail.com>
2020-11-06 21:36:12 +0900

    native test

--- Assets/5_XR_EDU/Scripts/System/NativeTest.cs
+++ Assets/5_XR_EDU/Scripts/System/NativeTest.cs
@@ -7,6 +7,10 @@ public class NativeTest : MonoBehaviour
    public void OnSaveScreenshotPress()
    {
       NativeToolkit.SaveScreenshot("MyScreenshot", "MyScreenshotFolder", "jpeg");
+
+      //NativeGallery.SaveImageToGallery("/MyScreenshotFolder/MyScreenshot.jpeg", "", "MyPic", null);
+
+      //NativeGallery.SaveImageToGallery(byte[] mediaBytes, string album, string filename, MediaSaveCallback callback = null)
    }
 
    // Start is called before the first frame update
