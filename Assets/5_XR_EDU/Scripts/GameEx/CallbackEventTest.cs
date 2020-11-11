33c4401f7884a30f998b1a1cc88dc76f31d55dfa
ckyun777 <ckyun777@gmail.com>
2020-11-11 12:13:50 +0900

    delegate callback test

---
+++ Assets/5_XR_EDU/Scripts/GameEx/CallbackEventTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void CallbackEvent();
+
+public class CallbackEventTest : MonoBehaviour
+{
+   private CallbackEvent callbackEvent = null;
+
+   void SomeFunc()
+	{
+      Debug.Log(" Function called ");
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        callbackEvent = SomeFunc;
+
+         callbackEvent();
+    }
+
+   
+}
